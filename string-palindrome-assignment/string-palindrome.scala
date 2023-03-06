38e0982dd565fa024cc6c00439c99541432fea07
sheshu2906 <sheshnath.yadav@knoldus.com>
2023-03-06 21:44:20 +0530

    string-palindrome file added

---
+++ string-palindrome-assignment/string-palindrome.scala
@@ -0,0 +1,48 @@
+import scala.io.StdIn
+
+object Palindrome_String extends App {
+  
+  // exception handling
+  try {
+    val user_input = StdIn.readLine()
+    if (user_input == "")
+      throw new Exception ("no input value")
+
+    //method for palindrome
+    def palindrome(user_input: String): Boolean = {
+      
+      // Define to the start and end of the string
+      var start_of_string = 0
+      var end_of_string = user_input.length - 1
+      
+      // Iterate through the string
+      while (start_of_string < end_of_string) {
+        
+        // Check if the characters at the start and end are equal
+        if (user_input(start_of_string) != user_input(end_of_string)) {
+          return false
+        }
+       
+        start_of_string += 1
+        end_of_string -= 1
+      }
+     
+      // return value to the method
+      true
+    }
+   
+    // check for palindrome
+    if (palindrome(user_input)) {
+      println("YES")
+   
+    } else {
+      print("NO")
+    }
+    
+  } catch {
+    case e: Exception =>
+      println("An error occurred: " + e.getMessage)
+  }
+  
+  
+}
